1384b99931483e8c8e7603c777f6063a3030c8b5
Jaya-S <jaivedgiri@gmail.com>
2017-07-05 21:01:56 +0530

    Create findevenoddnumbers.java

---
+++ findevenoddnumbers.java
@@ -0,0 +1,19 @@
+# shree
+ public class findevenoddnumbers {
+      
+
+public static void main( String []args){
+  
+  int []num=new int[]{1,2,3,4,5,6,7,8,9,10};
+  for(int i=0;i<num.length;i++){
+    if (num[i]%2==0) {
+        System.out.println(num[i]+"is a even number.");
+      } 
+      else {
+          System.out.println(  num[i]+"is a odd number.");
+      }
+      }
+      }
+      
+      
+      
